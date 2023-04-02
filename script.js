41d4b2a3077c7201117ed5145b571bab9256f39b
zain ahmed <zainahmed01997@gmail.com>
2023-04-02 19:05:22 +0530

    added initial html css js

---
+++ script.js
@@ -0,0 +1,12 @@
+function aboutHover() {
+    document.querySelector(".about").classList.add("about-active");
+  }
+  
+  function aboutLeave() {
+    document.querySelector(".about").classList.remove("about-active");
+  }
+  
+  function aboutTouch() {
+    document.querySelector(".about").classList.toggle("about-active");
+  }
+  
